cb837b563a61763a31d122f3b2963e09328ffae9
Nishan Rai <nishan2@nmsu.edu>
2026-06-28 09:11:08 -0600

    added runtime.txt file and related fixes

--- tools/search.py
+++ tools/search.py
@@ -1,9 +1,8 @@
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_core.tools import tool
 
-search = DuckDuckGoSearchRun()
-
 @tool
 def search_web(query: str) -> str:
     """Search the web for current information about any topic."""
+    search = DuckDuckGoSearchRun()
     return search.run(query)
